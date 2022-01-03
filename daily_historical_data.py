e4caed69889349b0e98d5d8b43b581a91aa35592
qwerty1434 <qwerty1434@naver.com>
2022-01-03 11:01:11 +0900

    Add daily_historical_data.py
    모든 종목의 1분봉 데이터를 수집하는 daily_historical_data.py 파일 업로드

---
+++ daily_historical_data.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 19 14:50:49 2021
+
+@author: 82104
+"""
+
+import pyupbit
+import os, glob
+import datetime
+import pandas as pd
+
+
+def rsi(data,period= 14):
+    delta = data['close'].diff()
+    ups,downs = delta.copy(), delta.copy()
+    ups[ups<0] = 0
+    downs[downs>0] = 0
+    AU = ups.ewm(com = period-1,min_periods = period).mean()
+    AD = downs.abs().ewm(com = period-1, min_periods = period).mean()
+    RS = AU/AD    
+    return pd.Series(100-(100/(1+RS)),name = 'RSI')
+
+
+
+#폴더이름
+yesterday = datetime.datetime.now() - datetime.timedelta(days = 1)
+yr = yesterday.year
+mth = yesterday.month
+dy = yesterday.day
+dir_name = str(yr)+str("-")+str(mth)+str("-")+str(dy)
+
+path = f"C:/Users/82104/Desktop/1분봉_매일데이터/{dir_name}"
+
+
+
+#폴더 만들기
+if os.path.isdir(path):
+    os.chdir(path)
+else:
+    os.mkdir(path)
+    os.chdir(path)
+
+#데이터 넣기
+coin_list = pyupbit.get_tickers(fiat="KRW")
+for coin in coin_list:   
+    df = pyupbit.get_ohlcv(coin, interval="minute1",count = 1441,to= datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day, 9, 1, 0)) #하루치 데이터
+    df['coin'] = coin    
+    df['rsi'] = rsi(df)
+    df.to_csv(f"{coin}.csv")
+
+#하나의 csv로 병합
+all_files = glob.glob(os.path.join(path, "*.csv"))
+df_from_each_file = (pd.read_csv(f, sep=',') for f in all_files)
+df_merged   = pd.concat(df_from_each_file, ignore_index=True)
+df_merged.to_csv( f"C:/Users/82104/Desktop/1분봉_매일데이터/{dir_name}_merged.csv")
+
+
+#왜 데이터가 다 없지? 9시부터 1440개 가져오는데 중간에 없는 데이터가 있어서 이전날9시보다 더 이전 데이터까지 들어오게 되네
